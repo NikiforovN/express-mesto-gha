e41cb523bdf88c7ae0615e0a97c39ef62a3b4131
Nikita Nikiforov <nikifor38@ya.ru>
2022-06-01 22:14:36 +0700

    BugsFixed

--- app.js
+++ app.js
@@ -24,7 +24,12 @@ mongoose.connect("mongodb://localhost:27017/mestodb");
 
 app.use(express.json());
 
-app.post("/signin", login);
+app.post("/signin", celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(4),
+  }),
+}), login);
 app.post("/signup", celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
