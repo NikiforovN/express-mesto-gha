7c8c8559f21eb2e4d3aebe7fe6ddbeece544a31a
Nikita Nikiforov <nikifor38@ya.ru>
2022-06-01 21:12:59 +0700

    BugsFixed

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.post("/signup", celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    email: Joi.string().required(),
+    email: Joi.string().required().email(),
     password: Joi.string().required().min(4),
   }).unknown(true),
 }), addUser);
