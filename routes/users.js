b848cd7f13c42de0db873bbbec620f2d292c5e34
Nikita Nikiforov <nikifor38@ya.ru>
2022-06-01 22:33:39 +0700

    BugsFixed

--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,7 @@ router.get("/", getUsers);
 router.get("/me", getCurrentUser);
 router.get("/:userId", celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().required.hex().length(24),
+    userId: Joi.string().required().hex().length(24),
   }),
 }), getUserById);
 
